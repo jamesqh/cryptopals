f417c432c19a5e7400394e39a08a7293f327b88b
James <james.q.hogan@gmail.com>
2017-03-26 16:20:12 +0100

    Fixed Jacobi symbol. FINALLY.

--- challenges/set5/challenge39.py
+++ challenges/set5/challenge39.py
@@ -3,7 +3,7 @@ I'm gonna implement Baillie-PSW primegen because RSA on its own is hardly fun"""
 
 import math
 
-from pdb import set_trace
+from sympy import isprime
 
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
           67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137,
@@ -15,11 +15,14 @@ primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
 
 
 def baillie_psw(candidate, trial_division_limit=541):
+    if candidate == 0:
+        return False
+    if candidate == 1:
+        return False
     if candidate == 2:
         return True
-    for prime in [x for x in primes if x <= trial_division_limit]:
+    for prime in [x for x in primes if x <= trial_division_limit and x != candidate]:
         if candidate % prime == 0:
-            print("trial division false")
             return False
     if not strong_fermat_test(candidate):
         print("fermat false")
@@ -69,7 +72,6 @@ def strong_fermat_test(candidate, base=2):
 
 
 def lucas_test(candidate, D, P, Q):
-    set_trace()
     assert D == P**2 - 4*Q
     assert gcd(candidate, Q) == 1, (Q, gcd(candidate, Q))
     n = candidate
@@ -109,33 +111,34 @@ def get_Un_and_Vn(n, P, Q):
             lucas_PQ[n] = (U_n, V_n)
             return lucas_PQ[n]
 
-# TODO: THIS JACOBI SYMBOL IS MESSED UP
+
 def jacobi(a, n):
     a = a % n
-    if a == 0:
-        return 0
-    if a == -1 % n:
-        if n == 1 % 4:
-            return 1
-        if n == 3 % 4:
-            return -1
-    if a == 2 % n:
-        if n == 1 % 8 or n == 7 % 8:
-            return 1
-        if n == 3 % 8 or n == 5 % 8:
-            return -1
-    if a == 1:
-        return 1
     if gcd(a, n) != 1:
         return 0
-    n, s = power_2_factor(n)  # such that old_n = n * 2**s
-    if s != 0:
-        return jacobi(2, n) ** s * jacobi(a, n)
-    else:
+    if a == 1:
+        return 1
+    if a == 2:
+        if n % 8 == 1 or n % 8 == 7:
+            return 1
+        if n % 8 == 3 or n % 8 == 5:
+            return -1
+        else:
+            print("bad stuff")
+    d, s = power_2_factor(a)
+    if s > 0:
+        return jacobi(d, n) * jacobi(2, n)**s
+    if n % 4 == 1 or a % 4 == 1:
         return jacobi(n, a)
+    if n % 4 == a % 4 == 3:
+        return -jacobi(n, a)
+    else:
+        print("bad stuff")
 
 
 def gcd(a, n):
+    if a == 0:
+        return 0
     while n != 0:
         t = n
         n = a % n
@@ -150,8 +153,5 @@ def power_2_factor(n):
         s += 1
     return n, s
 
-
-assert baillie_psw(11, trial_division_limit=1)
-
-for prime in primes:
-    assert baillie_psw(prime, trial_division_limit=1), prime
+for i in range(100000):
+    assert baillie_psw(i) == isprime(i), i

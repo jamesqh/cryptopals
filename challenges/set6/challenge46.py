cf2e6b4afffb60b44bdf4f23533e172b72401e6d
James <james.q.hogan@gmail.com>
2017-07-09 21:33:13 +0100

    46. Finally! The arithmetic is a trap

--- challenges/set6/challenge46.py
+++ challenges/set6/challenge46.py
@@ -1,9 +1,14 @@
 """Cryptopals set 6 challenge 46: RSA parity oracle
-This one is so good!"""
+This one is so good!
+The binary search uses REAL intervals not integer arithmetic,
+so we'll use the decimal module to get floats of sufficient precision.
+The lower and upper bounds both converge on the answer, one rounds up the other down."""
+
+from decimal import Decimal, getcontext
+from math import ceil
 
 from challenges.set5.challenge39 import (rsa_encrypt, rsa_decrypt,
                                          generate_rsa_key, int2bytes)
-from challenges.set5.challenge40 import rounded_integer_division
 
 
 class RSAParityOracle:
@@ -23,24 +28,24 @@ class RSAParityOracle:
 if __name__ == "__main__":
     oracle = RSAParityOracle()
     import binascii
-    cipher = oracle.encrypt_message(
-        binascii.a2b_base64("VGhhdCdzIHdoeSBJIGZvdW5kIHlvdSBkb24ndCBwbGF5IGFyb3"
-                            "VuZCB3aXRoIHRoZSBGdW5reSBDb2xkIE1lZGluYQ==")
-    )
-    #cipher = oracle.encrypt_message(b"hello")
+    msg = binascii.a2b_base64("VGhhdCdzIHdoeSBJIGZvdW5kIHlvdSBkb24ndCBwbGF5IGFy"
+                              "b3VuZCB3aXRoIHRoZSBGdW5reSBDb2xkIE1lZGluYQ==")
+    msg_hash = hash(msg)
+    cipher = oracle.encrypt_message(msg)
     register = cipher
     n = oracle.get_public_key().modulo
     e = oracle.get_public_key().key
     multiplier = oracle.encrypt_message(2)
-    lower_bound = 0
-    upper_bound = n
-    while lower_bound != upper_bound:
-        print(int2bytes(lower_bound))
-        print(int2bytes(upper_bound))
-        #register = (2**e)*register % n
+    getcontext().prec = n.bit_length() # set precision to be sufficient
+    lower_bound = Decimal(0)
+    upper_bound = Decimal(n)
+    for _ in range(n.bit_length()):
         register = multiplier * register % n
-        if oracle.parity_check(register):
-            lower_bound += rounded_integer_division(upper_bound - lower_bound, 2)
+        mid = (upper_bound + lower_bound) / Decimal(2)
+        if oracle.parity_check(register) == 1:
+            lower_bound = mid
         else:
-            upper_bound -= rounded_integer_division(upper_bound - lower_bound, 2)
-    print(int2bytes(upper_bound))
+            upper_bound = mid
+    assert int(ceil(lower_bound)) == int(upper_bound)
+    assert hash(int2bytes(int(upper_bound))) == msg_hash
+    print("Challenge complete")
